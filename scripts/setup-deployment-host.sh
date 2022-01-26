548de346982ad7a86d3fbdd681e9a95f70babb29
lnx1288 <20599452+lnx1288@users.noreply.github.com>
2022-01-26 17:10:56 +0100

    fixed script to launch deployment host
    fixed var/file naming issues

--- scripts/setup-deployment-host.sh
+++ scripts/setup-deployment-host.sh
@@ -8,11 +8,13 @@ ROOT_DIR=$PWD/../
 . ${ROOT_DIR}configs/vars
 
 # launch deployment host
-if lxc profile show "$DEP_HOST_NAME-profile" &> /dev/null; then
-    echo "ERROR: Profile exists, please delete $DEP_HOST_NAME-profile and retry"
+LXD_PROFILE="$DEP_HOST_NAME-lxd-profile"
+
+if lxc profile show "$LXD_PROFILE" &> /dev/null; then
+    echo "ERROR: Profile exists, please delete $LXD_PROFILE and retry"
     exit
 else
-    lxc profile create "$DEP_HOST_NAME-profile"
+    lxc profile create $LXD_PROFILE
 fi
-cat ${ROOT_DIR}configs/deployment-host-profile | lxc profile edit "$DEP_HOST_NAME-profile"
-lxc launch -p "$DEP_HOST_NAME-profile" ubuntu:$DEP_HOST_OS_RELEASE $DEP_HOST_NAME
+cat "${ROOT_DIR}configs/$LXD_PROFILE" | lxc profile edit $LXD_PROFILE
+lxc launch -p $LXD_PROFILE ubuntu:$DEP_HOST_OS_RELEASE $DEP_HOST_NAME
