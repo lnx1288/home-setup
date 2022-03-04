29a54eaf6c14a835447153dbaac827a8a1a11cc8
lnx1288 <20599452+lnx1288@users.noreply.github.com>
2022-03-04 23:28:20 +0100

    minor changes for compatibility with the tests

--- scripts/setup-deployment-host.sh
+++ scripts/setup-deployment-host.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# current dir shenanigans
-cd "$(dirname "$0")"  # cd into the script dir
-ROOT_DIR=$PWD/../
+repo_root_dir=$(find $HOME -name home-setup 2> /dev/null)
 
 # set up vars
-. ${ROOT_DIR}configs/vars
+source "$repo_root_dir/configs/vars"
 
 # launch deployment host
-LXD_PROFILE="$DEP_HOST_NAME-lxd-profile"
-
 if lxc profile show "$LXD_PROFILE" &> /dev/null; then
     echo "ERROR: Profile exists, please delete $LXD_PROFILE and retry"
     exit
 else
     lxc profile create $LXD_PROFILE
 fi
-cat "${ROOT_DIR}configs/$LXD_PROFILE" | lxc profile edit $LXD_PROFILE
+cat "$repo_root_dir/configs/$LXD_PROFILE" | lxc profile edit $LXD_PROFILE
 lxc launch -p $LXD_PROFILE ubuntu:$DEP_HOST_OS_RELEASE $DEP_HOST_NAME
+lxc list
